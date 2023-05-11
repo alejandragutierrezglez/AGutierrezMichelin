b8bca9a742337c71a6bcebae9af80dc9c22cc8e9
alejandragutierrezglez <alejandragutierrezglez2023@gmail.com>
2023-05-11 13:17:15 -0600

    Agregar archivos de proyecto.

---
+++ PL/Controllers/MapaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class MapaController : Controller
+    {
+        // GET: Mapa
+        public ActionResult GetMapa()
+        {
+            return View();
+        }
+       
+    }
+}
